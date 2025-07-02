250a481de51085e42cef9e7b997e337067e97b20
drushya <drushyareddy29@gmail.com>
2025-07-02 21:59:49 +0530

    added project 2: terraform backend setup with s3 and dynamodb

---
+++ terraform-backend-setup/backend-setup.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "us-east-1"
+ }
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-state-drushya-20250702"
+  force_destroy = true
+
+  versioning {
+  enabled = true
+ }
+
+ tags = {
+  Name = "Terraform State Bucket"
+ }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+   name   = "terraform-locks"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key     = "LockID"
+
+  attribute {
+  name = "LockID"
+  type = "S"
+} 
+  tags = {
+   Name = "Terraform Locks Table"
+ }
+}
